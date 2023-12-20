f66e05ebceebd9e7570263940944b5d6aef73378
yamamoto <ryutaro_yamamoto@narutech.co.jp>
2023-12-21 02:33:05 +0900

    完成

--- prisma-api-crud/routes/api.route.js
+++ prisma-api-crud/routes/api.route.js
@@ -1,31 +1,80 @@
 const router = require('express').Router();
-const {PrismaClient} = require('@prisma/client')
+const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
 router.get('/products', async (req, res, next) => {
- try {
-const
- } catch (error) {
-  next(error)
- }
+  try {
+    const products = await prisma.product.findMany({
+      include: { category: true },
+    })
+
+    const categories = await prisma.category.findMany({
+      include: { products: true },
+      })
+    res.json({ products,categories });
+  } catch (error) {
+    next(error)
+  }
 });
 
 router.get('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+ try {
+  const {id} = req.params
+  const product = await prisma.product.findUnique({
+    where:{
+      id: Number(id)
+    },
+    include: {category: true}
+  })
+  res.json(product)
+ } catch (error) {
+  
+ }
 });
 
 router.post('/products', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+  try {
+    const product = await prisma.product.create({
+      data: req.body,
+    })
+    res.json(product)
+  } catch (error) {
+    
+  }
 });
 
 router.delete('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
+  try {
+    const {id} = req.params
+    const deleteproduct = await prisma.product.delete({
+      where: {
+        id: Number(id),
+      }
+    })
+    res.json(deleteproduct)
+   } catch (error) {
+    
+   }
 });
 
 router.patch('/products/:id', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
-  
+ try {
+  const {id} = req.params
+  const product = await prisma.product.update({
+    where: {
+      id:Number(id),
+    },
+    data: req.body,
+    include: {
+      category: true,
+    }
+  })
+  res.json(product)
+ } catch (error) {
+  next(error)
+ }
+
 });
 
 module.exports = router;
